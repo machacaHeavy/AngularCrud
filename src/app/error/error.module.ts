41b830fd11ad0d648b55db4bf47234fc4fafc665
unknown <jsoliss@230177-96272775.coppel.com>
2017-05-05 12:09:03 -0600

    d

--- src/app/error/error.module.ts
+++ src/app/error/error.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ErrorComponent } from './error.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 
+
+const errorRoutes: Routes = [
+  { path: '', component: ErrorComponent, children: [
+    { path: '', component: NotfoundComponent }
+  ]}
+];
+
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule.forChild(errorRoutes)
   ],
   declarations: [ErrorComponent, NotfoundComponent]
 })
